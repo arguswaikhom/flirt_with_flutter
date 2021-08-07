98d3c88a05f9416813d3316d70d9eb5951e31ea3
Argus Waikhom <realarguswaikhom@gmail.com>
2021-08-07 10:59:17 +0530

    Refactoring project file and removing archived widgets and page files

--- lib/src/constants/app_nav_list.dart
+++ lib/src/constants/app_nav_list.dart
@@ -4,35 +4,9 @@ import 'package:flutter/material.dart';
 class AppNavList {
   static final List<ExpandableNavGroup> list = <ExpandableNavGroup>[
     ExpandableNavGroup(
-      label: "Test pages",
-      icon: Icons.list_alt_rounded,
-      children: [
-        TitleNav(
-          label: "Biting stream builder",
-          page: BitingStreamBuilderPage(),
-        ),
-      ],
-    ),
-    ExpandableNavGroup(
       label: AppString.titleNavTab,
       icon: Icons.tab,
       children: [
-        TitleNav(
-          label: AppString.titleIconTab,
-          page: IconTabPage(),
-        ),
-        TitleNav(
-          label: AppString.titleTextTab,
-          page: TextTabPage(),
-        ),
-        TitleNav(
-          label: AppString.titleIconAndTextTab,
-          page: IconAndTextTabPage(),
-        ),
-        TitleNav(
-          label: AppString.titleRoundedCornerWithBorderTab,
-          page: RoundedCornerWithBorderTabPage(),
-        ),
         TitleNav(
           label: AppString.titleToggleTab,
           page: ToggleTabPage(),
